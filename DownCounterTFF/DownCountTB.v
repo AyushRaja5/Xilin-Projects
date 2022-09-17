ec39a3958cce9500fbfe572eb2a017a79c3453f7
AYUSH RAJA <101151963+AyushRaja5@users.noreply.github.com>
2022-09-17 11:11:46 +0530

    Add files via upload

---
+++ DownCounterTFF/DownCountTB.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:32:09 11/02/2021
+// Design Name:   DownCounterM
+// Module Name:   D:/Xilin LAb/DownCounterTFF/DownCountTB.v
+// Project Name:  DownCounterTFF
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: DownCounterM
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module DownCountTB_v;
+
+	// Inputs
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire Q4;
+	wire Q3;
+	wire Q2;
+	wire Q1;
+
+	// Instantiate the Unit Under Test (UUT)
+	DownCounterM uut (
+		.clk(clk), 
+		.rst(rst), 
+		.Q4(Q4), 
+		.Q3(Q3), 
+		.Q2(Q2), 
+		.Q1(Q1)
+	);
+
+initial begin 
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+rst=1;
+#20;
+rst=0;
+end
+      
+endmodule
+
---
+++ DownCounterTFF/DownCounterM.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:52:08 11/02/2021 
+// Design Name: 
+// Module Name:    DownCounterM 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module tff(clk,t,rst,q,qbar);
+	input clk,rst,t;
+	output reg q;
+	output qbar;
+	assign qbar = ~q;
+	always @(posedge clk)
+	begin 
+		if(rst)
+			q<=4'b0;
+		else
+			if(t)
+				q <= ~q;
+			else
+				q <= q;
+	end
+endmodule
+
+
+module DownCounterM(clk, rst, Q4, Q3, Q2, Q1);
+   input clk;
+   input rst;
+   output Q4;
+   output Q3;
+   output Q2;
+   output Q1;
+	wire Q1_bar,Q2_bar,Q3_bar,Q4_bar;
+	
+	tff T1(clk,4'b1,rst,Q1,Q1_bar);
+	tff T2(clk,Q1_bar,rst,Q2,Q2_bar);
+	tff T3(clk,(Q2_bar & Q1_bar),rst,Q3,Q3_bar);
+	tff T4(clk,(Q3_bar & Q2_bar & Q1_bar),rst,Q4,Q4_bar);
+
+endmodule
