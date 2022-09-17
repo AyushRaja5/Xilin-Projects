2863b487f5fdaecaa79e9e35a100fc1f8cfb93e3
AYUSH RAJA <101151963+AyushRaja5@users.noreply.github.com>
2022-09-17 10:01:12 +0530

    Add files via upload

---
+++ FullAdderUsingMuxCaseStatement/FAuseMUXTB.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   02:49:15 11/23/2021
+// Design Name:   FAusngMUX4
+// Module Name:   D:/Xilin LAb/FullAdderUsingMuxCaseStatement/FAuseMUXTB.v
+// Project Name:  FullAdderUsingMuxCaseStatement
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FAusngMUX4
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module FAuseMUXTB_v;
+
+	// Inputs
+	reg A;
+	reg B;
+	reg Cin;
+
+	// Outputs
+	wire Sum;
+	wire Carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	FAusngMUX4 uut (
+		.A(A), 
+		.B(B), 
+		.Cin(Cin), 
+		.Sum(Sum), 
+		.Carry(Carry)
+	);
+
+	initial begin
+		// Initialize Inputs
+		A = 0;
+		B = 0;
+		Cin = 0;
+
+		#10;
+        
+		// Add stimulus here
+		#10 A=0; B=1; Cin=0;
+		#10 A=0; B=1; Cin=0;
+		#10 A=1; B=0; Cin=0;
+		#10 A=1; B=1; Cin=0;
+		
+		#10 A=0; B=0; Cin=1;
+		#10 A=0; B=1; Cin=1;
+		#10 A=1; B=0; Cin=1;
+		#10 A=1; B=1; Cin=1;
+	end
+      
+endmodule
+
---
+++ FullAdderUsingMuxCaseStatement/FAusngMUX4.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    02:32:07 11/23/2021 
+// Design Name: 
+// Module Name:    FAusngMUX4 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mux(z,d0,d1,d2,d3,s0,s1);
+input d0,d1,d2,d3,s0,s1;
+output z;
+reg z;
+always @(d0 or d1 or d2 or d3 or s0 or s1)
+ begin
+ case({s0,s1})
+ 2'b00: z<=d0;
+ 2'b01: z<=d1;
+ 2'b10: z<=d2;
+ 2'b11: z<=d3;
+ endcase
+ end
+ endmodule
+
+module FAusngMUX4(A, B, Cin, Sum, Carry);
+    input A;
+    input B;
+    input Cin;
+    output Sum;
+    output Carry;
+	wire  w1;
+	 assign w1=~A;
+	mux m1(Sum,A,w1,w1,A,Cin,B);
+	mux m2(Carry,0,A,A,1,Cin,B);
+	
+endmodule
